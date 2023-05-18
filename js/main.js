8d78b6b93465712d611cedf2e9c056ac341596a8
alexander <alexandermackenzie3775@gmail.com>
2023-05-17 21:04:25 -0400

    Added domain-block component updated input-groups

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+window.addEventListener('resize', function() {
+  var inputs = document.querySelectorAll('.scalable-placeholder');
+  for (var i = 0; i < inputs.length; i++) {
+    var input = inputs[i];
+    if (window.innerWidth > 500) {
+      input.placeholder = 'Enter domain name here...';
+    } else {
+      input.placeholder = 'Domain name...';
+    }
+  }
+});
